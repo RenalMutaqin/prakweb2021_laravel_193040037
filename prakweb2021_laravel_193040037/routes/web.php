709598247dbb4a593bd98c6e81fc4f616e60cfb5
RenalMutaqin <renalmutaqin8@gmail.com>
2021-10-07 12:16:53 +0700

    Menyelesaikan video ke 3

--- prakweb2021_laravel_193040037/routes/web.php
+++ prakweb2021_laravel_193040037/routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "nama" => "Renal Mutaqin",
+        "email" => "renalmutaqin8@gmail.com",
+        "image" => "foto.jpeg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
