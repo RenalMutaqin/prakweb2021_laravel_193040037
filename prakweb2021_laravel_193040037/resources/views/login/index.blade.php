0b8b94318a5df97fbae8d24fcf989a1f0bc3f7b6
RenalMutaqin <renalmutaqin8@gmail.com>
2021-10-28 15:53:36 +0700

    Menyelesaikan video user registration

--- prakweb2021_laravel_193040037/resources/views/login/index.blade.php
+++ prakweb2021_laravel_193040037/resources/views/login/index.blade.php
@@ -8,11 +8,13 @@
       {{ session('success') }}
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
-@endif
-  $if(session()->has('success'))
+
+
+  @if(session()->has('success'))
     <main class="form-signin">
     <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
   <form>
+    @endif
     
 
     <div class="form-floating">
