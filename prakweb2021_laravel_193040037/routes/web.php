a4723884700c903af0020b49a023c023eff13cd9
RenalMutaqin <renalmutaqin8@gmail.com>
2021-11-18 14:36:54 +0700

    Menyelesaikan laravel bagian 5, video 17

--- prakweb2021_laravel_193040037/routes/web.php
+++ prakweb2021_laravel_193040037/routes/web.php
@@ -3,10 +3,12 @@
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +21,10 @@ use App\Http\Controllers\RegisterController;
 |
 */
 
-Route::get('/', function () {
-    return view('home',[
-        "title" => "home",
-        "active" => "home"
+Route::get('/', function() {
+    return view('home', [
+        "title" => "Home",
+        'active' => 'home'
     ]);
 });
 Route::get('/about', function () {
@@ -36,39 +38,50 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', [PostController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-
-
-Route :: get('posts/{post:slug}',[PostController::class, 'show']);
-
-Route :: get('/categories', function(){
-    return view('cayegory', [
-        'title'=> 'Post Categories',
-        "active" => "categories",
-        'categories'=> Category::all()
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'active' => 'categories',
+        'categories' => Category::all()
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts', [
-        'title'=>"Post By Category : $category->name",
-        "active" => "categories",
-        'posts'=> $category->posts->load('category', 'author')
-    ]);
-});
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-route::get('/authors/{author:username}' , function(User $author){
-    return view('posts', [
-        'title'=> "Post By Author : $author->name",
-        "active" => "post",
-        'posts'=> $author->posts->load('category','author')
-    ]);
-});
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/login',[LoginController::class, 'index'])->name('login')->middleware('guest');
-Route::post('/login',[LoginController::class, 'authenticate']);
-Route::post('/logout',[LoginController::class, 'logout']);
-Route::get('/register',[RegisterController::class, 'index'])->middleware('guest');
-Route::post('/register',[RegisterController::class, 'store']);
-Route::get('/dashboard',[DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+
+
+
+
+
+
+
+// Route::get('/categories/{category:slug}', function(Category $category){
+//     return view('posts', [
+//         'title' => "Post by category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
+
+// Route::get('/authors/{author:username}', function(User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author->name",
+//         'active' => 'author',
+//         'posts' => $author->posts->load('category', 'author')
+//     ]);
+// }); 
